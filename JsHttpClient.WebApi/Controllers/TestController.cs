74fa6857dec96da798fc4d028bb2fd03f2be8a74
jason <jason@A218>
2020-06-21 16:55:05 +0800

    Example

---
+++ JsHttpClient.WebApi/Controllers/TestController.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JasonSoft.Net.JsHttpClient.Http;
+
+namespace JsHttpClient.WebApi.Controllers
+{
+    /// <summary>
+    /// Add by Jason.Song（成长的小猪） on 2019/04/24
+    /// http://blog.csdn.net/jasonsong2008
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IJsHttpClient _client;
+
+        /// <summary>
+        /// 实例化
+        /// Add by Jason.Song（成长的小猪） on 2019/04/24
+        /// </summary>
+        /// <param name="client"></param>
+        public TestController(IJsHttpClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Asynchronous request test
+        /// 异步请求测试
+        /// Add by Jason.Song（成长的小猪） on 2019/04/24
+        /// http://blog.csdn.net/jasonsong2008
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("HttpAsync")]
+        public async Task<IActionResult> HttpAsync()
+        {
+            const string urlString = "https://blog.csdn.net/jasonsong2008";
+            var request = new JsHttpRequest {Uri = urlString};
+            // request.Method = HttpMethod.Get;
+            // request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*";
+            // request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36";
+            // request.Referer = "https://blog.csdn.net/";
+            // request.Host = "blog.csdn.net";
+            // request.Cookie = "";
+            // request.Timeout = 30;
+            // request.Add("Upgrade-Insecure-Requests", "1");
+
+            var response = await _client.SendAsync(request);
+            //response.Cookie
+            //response.ResultByte
+            return Content(response.Html, "text/html; charset=utf-8");
+        }
+
+        /// <summary>
+        /// Synchronous request test
+        /// 同步请求测试
+        /// Add by Jason.Song（成长的小猪） on 2019/04/24
+        /// http://blog.csdn.net/jasonsong2008
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("HttpSync")]
+        public IActionResult HttpSync()
+        {
+            const string urlString = "https://blog.csdn.net/jasonsong2008";
+            var request = new JsHttpRequest {Uri = urlString};
+            // request.Method = HttpMethod.Get;
+            // request.Method = HttpMethod.Post;
+            // request.ContentType = "application/json";
+            // request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*";
+            // request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36";
+            // request.Referer = "https://blog.csdn.net/";
+            // request.Host = "blog.csdn.net";
+            // request.Cookie = "";
+            // request.Timeout = 30;
+            // request.Add("Upgrade-Insecure-Requests", "1");
+
+            var response = _client.Send(request);
+            // response.StatusCode
+            // response.Cookie
+            // response.ResultByte
+            return Content(response.Html, "text/html; charset=utf-8");
+        }
+    }
+}
---
+++ JsHttpClient.WebApi/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+//引用 JsHttpClient
+using JasonSoft.Net.JsHttpClient.Extensions;
+
+namespace JsHttpClient.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // 添加 JsHttpClient 
+            // Add by Jason.Song（成长的小猪） on 2019/04/23
+            // services.AddJsHttpClient();
+            // 是否允许自动重定向功能，如果不需要，请使用以下方式，默认允许为 true
+            // services.AddJsHttpClient(new JsHttpClientOptions{ AllowAutoRedirect = false });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            { 
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
